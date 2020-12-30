5a48c1b72ed3c3cef7f27f7ffdbfab0e4f3f8398
mjkim <mjkim103301@gmail.com>
2020-12-30 22:48:35 +0900

    refactor: MethodRefactoring
    indent 2에 맞추기 위해서 메소드 작은 기능으로 쪼갬
    상수를 전역변수에 저장함.

--- src/main/java/숫자야구게임/NumberBaseball.java
+++ src/main/java/숫자야구게임/NumberBaseball.java
@@ -4,86 +4,98 @@ import java.util.Random;
 import java.util.Scanner;
 
 class Solution{
+    private static final int NUM_BOUND=10;
+    private static final int NUM_SIZE=3;
+    private static final int ZERO_UNICODE=48;
+
+    private int[] computer;
+    private int[] user;
+    private int[] state;
+
     public enum Status{
         STRIKE,
         BALL,
         OUT
     }
 
-    int[] notOut(int[] computer, int[]user, int ui, int ci, int[] state){
-        if((ui==ci)||(user[ui]==computer[ui])){
-            state[Status.STRIKE.ordinal()]++;
-        }else {
-            state[Status.BALL.ordinal()]++;
-        }
-        return state;
+   public boolean getOutResult(int userIndex, int computerIndex){
+       if(user[userIndex]==computer[computerIndex]){
+           if(userIndex==computerIndex){
+               state[Status.STRIKE.ordinal()]++;
+           }else {
+               state[Status.BALL.ordinal()]++;
+           }
+           return false;
+       }
+       return true;
     }
 
-    public int[] findAns(int[] computer, int[]user){
-        int[] state=new int[3];
+    public void findAns( ){
+        state=new int[NUM_SIZE];
         boolean isOut=true;
-        for(int ui=0;ui<3;ui++){
-            for(int ci=0;ci<3;ci++){
-                if(user[ui]==computer[ci]){
-                    state=notOut(computer, user, ui, ci, state);
-                    isOut=false;
-                    break;
-                }
+        for(int ui=0;ui<NUM_SIZE;ui++){
+            for(int ci=0;ci<NUM_SIZE;ci++){
+                isOut=getOutResult(ui, ci);
+                if(isOut==false)break;
             }
             if(isOut){
                 state[Status.OUT.ordinal()]++;
             }
-            isOut=true;
         }
-        return state;
+     
     }
 
-    public int[] computerGenerator(){
-        Random generator=new Random();
-        int[] computer=new int[3];
-        for(int i=0;i<3;i++){
-            computer[i]=generator.nextInt(10);
+    public void computerGenerator() {
+        Random generator = new Random();
+        computer = new int[NUM_SIZE];
+        for (int i = 0; i < NUM_SIZE; i++) {
+            computer[i] = generator.nextInt(NUM_BOUND);
         }
-        return computer;
     }
-
-    public void solution(int[] computer){
-
-        int []user=new int[3];
-        String userNumber;
-        int []state;
+    public boolean isFinish(){
         int choice;
+        Scanner scan=new Scanner(System.in);
+        if(state[Status.STRIKE.ordinal()]>0){
+            System.out.print(state[Status.STRIKE.ordinal()]+"스트라이크 ");
+        }
+        if(state[Status.BALL.ordinal()]>0){
+            System.out.print(state[Status.BALL.ordinal()]+"볼 ");
+        }
+        if(state[Status.OUT.ordinal()]>0){
+            System.out.print(state[Status.OUT.ordinal()]+"아웃 ");
+        }
+        System.out.println("\n");
+        if(state[Status.STRIKE.ordinal()]==NUM_SIZE){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료\n");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+            choice=scan.nextInt();
+
+            if(choice==2){
+                System.out.println("게임을 종료합니다.\n");
+                return true;
+            }else{
+                computerGenerator();
+            }
+        }
+        return false;
+    }
+    public void solution(){
+
+        user=new int[NUM_SIZE];
+        String userNumber;
+
         while(true){
             Scanner scan=new Scanner(System.in);
             System.out.println("숫자를 입력해주세요: ");
             userNumber=scan.nextLine();
-            for(int i=0;i<3;i++){
-                user[i]=userNumber.charAt(i)-48;
+            for(int i=0;i<NUM_SIZE;i++){
+                user[i]=userNumber.charAt(i)-ZERO_UNICODE;
             }
-            state=findAns(computer, user).clone();
+            findAns();
+            if(isFinish()==true){
+                return;
+            }
 
-            if(state[Status.STRIKE.ordinal()]>0){
-                System.out.print(state[Status.STRIKE.ordinal()]+"스트라이크 ");
-            }
-            if(state[Status.BALL.ordinal()]>0){
-                System.out.print(state[Status.BALL.ordinal()]+"볼 ");
-            }
-            if(state[Status.OUT.ordinal()]>0){
-                System.out.print(state[Status.OUT.ordinal()]+"아웃 ");
-            }
-            System.out.println("\n");
-            if(state[Status.STRIKE.ordinal()]==3){
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료\n");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
-                choice=scan.nextInt();
-
-                if(choice==2){
-                    System.out.println("게임을 종료합니다.\n");
-                    return;
-                }else{
-                    computer=computerGenerator();
-                }
-            }
         }
     }
 }
@@ -91,10 +103,11 @@ class Solution{
 public class NumberBaseball {
     public static void main(String[] args){
         Solution solution=new Solution();
-        int[]computer=solution.computerGenerator();
-        computer=new int[]{1,2,3};
-        solution.solution(computer);
+        solution.computerGenerator();
+        solution.solution();
 
     }
 }
+
+
 
