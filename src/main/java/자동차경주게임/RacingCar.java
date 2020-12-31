304f6e540c94961e960b99ffbd77a92c203d0a12
mjkim <mjkim103301@gmail.com>
2020-12-31 14:21:42 +0900

    refactor: RacingCar refactoring
    함수 15줄 이하로 분리

--- src/main/java/자동차경주게임/RacingCar.java
+++ src/main/java/자동차경주게임/RacingCar.java
@@ -11,6 +11,7 @@ class Car{
         this.location=location;
         this.random=random;
     }
+
 }
 class Solution{
     private static final int NUM_BOUND=10;
@@ -18,79 +19,70 @@ class Solution{
     ArrayList<Car> cars=new ArrayList<>();
 
     int N;
+    int numberOfCars=0;
 
     void init(String carNames) {
-
         String carName;
         int division = -1;
-        int lastDivision = 0;
-
-         do{
+        int lastDivision;
+        do {
             lastDivision = division + 1;
             division = carNames.indexOf(",", lastDivision);
             if (division > 0) {
                 carName = carNames.substring(lastDivision, division);
                 cars.add(new Car(carName, 0, 0));
             }
-
-        }while (division != -1);
+        } while (division != -1);
         carName = carNames.substring(lastDivision);
         cars.add(new Car(carName, 0, 0));
+        numberOfCars = cars.size();
     }
+
     public void run(){
-        int carNum=cars.size();
         Random generator = new Random();
 
-        for (int i = 0; i < carNum; i++) {
+        for (int i = 0; i < numberOfCars; i++) {
             cars.get(i).random= generator.nextInt(NUM_BOUND);
             if(cars.get(i).random>3){
                 cars.get(i).location++;
             }
         }
     }
-    public int findWinner(){
-       //cars location순서대로 정렬
-        int winnerIndex=0;
-        int carNum=cars.size();
+    public void sort(){
         Collections.sort(cars, new Comparator<Car>() {
             @Override
             public int compare(Car car, Car t1) {
                 return t1.location-car.location;
             }
         });
+    }
 
-        for(int i=1;i<carNum;i++){
+    public int findWinner(){
+        int winnerIndex=0;
+
+        sort();
+        for(int i=1;i<numberOfCars;i++){
             if(cars.get(winnerIndex).location==cars.get(i).location){
                 winnerIndex++;
-            }else{
-                break;
+                continue;
             }
+            break;
         }
         return winnerIndex;
     }
-    public void solution(){
-        String carNames;
-        int winnerIndex=0;
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        carNames = scanner.nextLine();
-        init(carNames);
 
-        System.out.println("\n시도할 회수는 몇회인가요?");
-        N=scanner.nextInt();
-
-        System.out.println("\n실행결과");
-        for(int i=0;i<N;i++){
-            run();
-            for(int j=0;j<cars.size();j++){
-                System.out.print(cars.get(j).name+": ");
-                for(int k=0;k<cars.get(j).location;k++){
-                    System.out.print("-");
-                }
-                System.out.print("\n");
+    public void printLocation(){
+        for(int j=0;j<numberOfCars;j++){
+            System.out.print(cars.get(j).name+": ");
+            for(int k=0;k<cars.get(j).location;k++){
+                System.out.print("-");
             }
             System.out.print("\n");
         }
-        winnerIndex=findWinner();
+        System.out.print("\n");
+    }
+
+    public void printWinner(int winnerIndex){
         for(int j=0;j<=winnerIndex;j++){
             if(j>0){
                 System.out.print(", ");
@@ -100,7 +92,26 @@ class Solution{
         }
         System.out.println("가 최종 우승했습니다.");
     }
+
+    public void solution(){
+        String carNames;
+        int winnerIndex;
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        carNames = scanner.nextLine();
+
+        init(carNames);
+        System.out.println("\n시도할 회수는 몇회인가요?");
+        N=scanner.nextInt();
+        System.out.println("\n실행결과");
+        for(int i=0;i<N;i++){
+            run();
+            printLocation();
+        }
+        winnerIndex=findWinner();
+        printWinner(winnerIndex);
+    }
 }
+
 public class RacingCar {
     public static void main(String[] args){
         Solution sol=new Solution();
